e07f95cdfa440c17afb73c84b9e68b982cd203cb
PronayBormon <pronaykumarbormon@gmail.com>
2024-09-25 17:58:07 +0600

    git push

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.btn_sidebar').click(function () {
+        $('.dashboard_sidebar').toggleClass("show");
+    })
+})
+
+// $(document).ready(function () {
+//     $('#myButton').click(function () {
+//         $('#inactiveDiv').toggle();
+//         $('#activeDiv').toggle();
+//     });
+// });
